4fc137ed348eb42187937a2625a2ead2973b9d82
nkakhidze <nick200069@gmail.com>
2026-05-21 16:14:46 +0700

    task5 пузырьковая сортировка + сортировка вставками

--- Task5_Sorting.py
+++ Task5_Sorting.py
@@ -1,17 +1,13 @@
 # Реализуйте:
 # 1. Пузырьковую сортировку
-
 def sort_bubbling(massiv:list):
     for iteraczia in range(len(massiv)-1):
         schtchk = 0
-        i0 = 0
-        i1 = 1
-        for i in range(len(massiv)-1 - iteraczia):
-            if massiv[i0] > massiv[i1]:
-                massiv[i0], massiv[i1] = massiv[i1], massiv[i0]
+        for i in range(len(massiv) - 1 - iteraczia):
+            if massiv[i] > massiv[i + 1]:
+                massiv[i], massiv[i + 1] = massiv[i + 1], massiv[i]
                 schtchk += 1
-            i0 += 1
-            i1 += 1
+            i+= 1
         if schtchk == 0:
             return massiv
     return massiv
@@ -19,8 +15,19 @@ def sort_bubbling(massiv:list):
 
 
 # 2. Сортировку вставками
+def sort_pasting(massiv:list):
+    for i in range(1, len(massiv)):
+        while (massiv[i] < massiv[i - 1]) and (i > 0):
+            massiv[i - 1], massiv[i] = massiv[i], massiv[i - 1]
+            i -= 1
+    return massiv
+
+
+
+
+
 # 3. Сравните их с встроенной функцией sorted()
 # 4. Измерьте время выполнения для разных размеров входных данных
 
-a = [1, 0, 2, 5, 0, 12]
-print(sort_bubbling(a))
+a = [1, 0, 24, 2, 5, 0, 12]
+print(sort_pasting(a))
